53996ae8b7751dc9e308083d04c331e120ad853e
Michael Fitzmayer <mail@michael-fitzmayer.de>
2023-11-09 16:07:09 +0100

    Delete redundant files

--- firmware/Bootloader Ready/Drivers/lsm6ds3tr-c-pid/lsm6ds3tr-c_reg.d
+++
@@ -1,4 +0,0 @@
-Drivers/lsm6ds3tr-c-pid/lsm6ds3tr-c_reg.o: \
- ../Drivers/lsm6ds3tr-c-pid/lsm6ds3tr-c_reg.c \
- ../Drivers/lsm6ds3tr-c-pid/lsm6ds3tr-c_reg.h
-../Drivers/lsm6ds3tr-c-pid/lsm6ds3tr-c_reg.h:
